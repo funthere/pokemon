9601367a41a48ea7ed0db57b140e8bc08db80080
Suvander <suvander@gmail.com>
2023-11-24 16:43:21 +0700

    CSS improvement

--- src/Pokemon/Pokemon.js
+++ src/Pokemon/Pokemon.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Pokemon = ({ pokemon, onClick, buttonLabel }) => (
   <li key={pokemon.name}>
         <img alt={pokemon.name} src={pokemon.image}></img>
-        {pokemon.name}
+        <span className="pokemon-name">{pokemon.name}</span>
         <br></br>
         <button onClick={onClick(pokemon)}>{buttonLabel}</button>
   </li>
